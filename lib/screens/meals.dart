5df0a733ad1449852fc4720c6cebe664810b8f2b
SarahAmr <sab@blueribbon-inc.com>
2024-08-20 14:41:36 +0300

    Initial commit

---
+++ lib/screens/meals.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_details.dart';
+import 'package:meals/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  final String? title;
+  final List<Meal> meals;
+
+  const MealsScreen({
+    super.key,
+    this.title,
+    required this.meals,
+  });
+
+  void _selectMeal(BuildContext context, Meal meal) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (ctx) => MealDetailsScreen(
+                  meal: meal,
+                )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh ... nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onSurface,
+                ),
+          ),
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+          itemCount: meals.length,
+          itemBuilder: (context, index) => MealItem(
+                meal: meals[index],
+                onSelectMeal: (meal) {
+                  _selectMeal(context, meals[index]);
+                },
+              ));
+    }
+
+    if (title != null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(title!),
+        ),
+        body: content,
+      );
+    } else {
+      return content;
+    }
+  }
+}
